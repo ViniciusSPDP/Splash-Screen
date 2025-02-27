52c89dde03ad777af3e609fc7069a2d2293d492e
S4R41VA <vinicius_saraiva2012@hotmail.com>
2025-02-27 19:32:32 -0300

    Adiciona Splash Screen

--- App.js
+++ App.js
@@ -1,11 +1,66 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { View, StyleSheet, Platform, Text, Dimensions } from 'react-native';
+import { Video } from 'expo-av';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const videoRef = useRef(null);
+  const [appIsReady, setAppIsReady] = useState(false);
+  const [videoFinished, setVideoFinished] = useState(false);
+
+  useEffect(() => {
+    if (Platform.OS !== 'web') {
+      async function prepare() {
+        try {
+          await SplashScreen.preventAutoHideAsync();
+          // Simula um carregamento
+          await new Promise(resolve => setTimeout(resolve, 2000));
+        } catch (e) {
+          console.warn(e);
+        } finally {
+          setAppIsReady(true);
+          await SplashScreen.hideAsync();
+        }
+      }
+
+      prepare();
+    } else {
+      // No ambiente web, apenas define o app como pronto
+      setAppIsReady(true);
+    }
+  }, []);
+
+  // Função para detectar quando o vídeo termina
+  const handlePlaybackStatusUpdate = (status) => {
+    if (status.didJustFinish && !status.isLooping) {
+      setVideoFinished(true); // Marca o vídeo como finalizado
+    }
+  };
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {Platform.OS !== 'web' && !videoFinished && (
+        <Video
+          ref={videoRef}
+          source={require('./assets/Logo.mp4')}
+          style={styles.video}
+          resizeMode="cover"
+          shouldPlay
+          isLooping={false} // Remove o loop
+          onPlaybackStatusUpdate={handlePlaybackStatusUpdate} // Detecta quando o vídeo termina
+        />
+      )}
+      {videoFinished && (
+        <View style={styles.content}>
+          <Text>Criando uma Splash Screen</Text>
+          <StatusBar style="auto" />
+        </View>
+      )}
     </View>
   );
 }
@@ -13,8 +68,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  video: {
+    width: '100%', // Ocupa toda a largura da tela
+    height: '100%', // Ocupa toda a altura da tela
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
